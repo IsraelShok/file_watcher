f00c47ebe8d0a2859b675e783ca548bd650aeb6d
Israel Shok <israelshok@gmail.com>
2023-03-05 16:30:36 +0200

    Add files via upload

--- Services/Watcher/HandleEvent.py
+++ Services/Watcher/HandleEvent.py
@@ -11,9 +11,6 @@ class HandleEvent(FileSystemEventHandler):
     def __init__(self, watch_folder):
         self.watch_folder = watch_folder
 
-        # Create rabbitmq connection to send the message
-        self.connection, self.channel = get_rabbitmq_connection()
-
     def on_created(self, event):
         self.print_event(event)
         if not event.is_directory:
@@ -33,15 +30,21 @@ class HandleEvent(FileSystemEventHandler):
     def print_event(event):
         print(f'Capture event: {event}')
 
-    def send_message(self, filepath: str, event_type: str):
+    @staticmethod
+    def send_message(filepath: str, event_type: str):
+        # Create rabbitmq connection to send the message
+        connection, channel = get_rabbitmq_connection()
+
         message = {
             FILE_PATH: filepath,
             EVENT_TYPE: event_type
         }
 
         # Publish the message to rabbitmq
-        self.channel.basic_publish(exchange='',
-                                   routing_key=os.getenv(RABBITMQ_QUEUE),
-                                   body=json.dumps(message))
+        channel.basic_publish(exchange='',
+                              routing_key=os.getenv(RABBITMQ_QUEUE),
+                              body=json.dumps(message))
 
-        print(f'Sent message: {message}')
+        connection.close()
+
+        print(f'Sent message to rabbitMQ: {message}')
